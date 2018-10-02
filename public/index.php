494c47cfb80e14dd3f0d4cf31dd1b6882c439f80
Ruslan Knyazev <agrry@yandex.ru>
2018-10-02 10:01:25 +0500

    initial commit

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+use Framework\Http\Request;
+
+chdir(dirname(__DIR__));
+require('vendor/autoload.php');
+
+
+### Initialization
+$request = new Request();
+
+### Action
+$name = $request->getQueryParams()['name'] ?? 'Guest';
+
+echo 'Hello, ' . $name . '! Your lang is ' . $lang;
