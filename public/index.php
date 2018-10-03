b74161e8052b72da8ba399cc3e53427f232df577
Ruslan Knyazev <agrry@yandex.ru>
2018-10-03 17:56:58 +0500

    Wrap actions in callbacks

--- public/index.php
+++ public/index.php
@@ -2,9 +2,11 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response\HtmlResponse;
 use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
 use Zend\Diactoros\ServerRequestFactory;
+use Zend\Diactoros\Response\JsonResponse;
 
 chdir(dirname(__DIR__));
 require('vendor/autoload.php');
@@ -15,14 +17,41 @@ $request = ServerRequestFactory::fromGlobals();
 
 ### Action
 $path = $request->getUri()->getPath();
+$action = null;
 
 if ($path === '/') {
-    $name = $request->getQueryParams()['name'] ?? 'Guest';
-    $response = new HtmlResponse('Hello, ' . $name . '!');
+    $action = function (ServerRequestInterface $request) {
+        $name = $request->getQueryParams()['name'] ?? 'Guest';
+        return new HtmlResponse('Hello, ' . $name . '!');
+    };
 } elseif ($path === '/about') {
-    $response = new HtmlResponse('I am simple site');
+    $action = function (ServerRequestInterface $request) {
+        return new HtmlResponse('I am simple site');
+    };
+} elseif ($path === '/blog') {
+    $action = function (ServerRequestInterface $request) {
+        return new JsonResponse([
+            ['id' => 2, 'title' => 'The Second Post'],
+            ['id' => 1, 'title' => 'The First Post']
+        ]);
+    };
+} elseif (preg_match('#^/blog/(?P<id>\d+)$#i', $path, $matches)) {
+    $request = $request->withAttribute('id', $matches['id']);
+
+    $action = function (ServerRequestInterface $request) {
+        $id = $request->getAttribute('id');
+        if ($id > 2) {
+            return new JsonResponse(['error' => 'Undefined page'], 404);
+        }
+
+        return new JsonResponse(['id' => $id, 'title' => 'Post #' . $id]);
+    };
+}
+
+if ($action) {
+    $response = $action($request);
 } else {
-    $response = new HtmlResponse('Undefined page', 404);
+    $response = new JsonResponse(['error' => 'Undefined page'], 404);
 }
 
 ### Postprocessing
